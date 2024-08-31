3d20c8080f05aeb448ba970c7c37ea534da815b5
Tuluobo <tuluobo@vip.qq.com>
2024-08-31 17:23:13 +0800

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function IndexPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 flex w-full max-w-5xl flex-col items-center justify-between font-mono text-sm">
+        <h1>Hello, Next js & Shadcn UI & Next Auth</h1>
+        <br />
+        <button>Start</button>
+      </div>
+    </main>
+  );
+}

0a9cdbd62e21b4f7a1fdf759406a68fe839f1936
Tuluobo <tuluobo@vip.qq.com>
2024-08-31 22:00:09 +0800

    feat: Add shadcn ui & theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/theme-toggle";
+
 export default function IndexPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 flex w-full max-w-5xl flex-col items-center justify-between font-mono text-sm">
-        <h1>Hello, Next js & Shadcn UI & Next Auth</h1>
-        <br />
-        <button>Start</button>
-      </div>
-    </main>
+    <>
+      <header className="flex h-24 items-center justify-center">
+        <div className="flex w-full max-w-5xl items-center justify-between">
+          <div>
+            <h1 className="text-xl">Next.js</h1>
+          </div>
+          <ThemeToggle />
+        </div>
+      </header>
+      <main className="flex flex-col items-center justify-between p-24">
+        <div className="z-10 flex w-full max-w-5xl flex-col items-center justify-between font-mono text-sm">
+          <h1>Hello, Next js & Shadcn UI & Next Auth</h1>
+          <br />
+          <Button>Start</Button>
+        </div>
+      </main>
+    </>
   );
 }
